a2325cddf0695688e9347b90934fa4cee0e50e70
arminepanosyan <118587113+arminepanosyan@users.noreply.github.com>
2022-11-19 12:51:36 +0400

    Add files via upload

---
+++ ufar_home/script.js
@@ -0,0 +1,63 @@
+// You must solve other exercises according to the example below.
+// Also, don't forget send me your github pages url like yourUsername.github.io
+
+// 5. Given two numbers print 1 if one of them is divisible by the other one, otherwise print 0.
+
+// defining variables to use
+// let number1
+// let number2
+
+// // checking condition
+// if (number1 / number2 === 0 || number2 / number1 === 0) {
+//   console.log(1)
+// } else {
+//   console.log(0)
+// }
+
+
+//1. Declare 2 variables a and b , such that a > b , and define values for them. output their
+
+// let a = 5
+// let b = 3
+
+// //a
+// console.log(a + b);
+// //b
+// console.log(a - b);
+// //c
+// console.log(a / b);
+// //d
+// console.log(a % b);
+
+
+//2.Print console.log() your name and age in the following format:"My name is ___, I am ___." Name and age
+//should be  variables with defined values
+
+// let name = "Armine";
+// let age = 18;
+
+// console.log(`My name is ${name}, I am ${age}.`);
+
+//3 Check whether a given number is negative. Print 'yes', if it is negative , print 'no' otherwise
+
+// let number = -10
+
+// if(number<0){
+//   console.log("yes");
+// }
+// else{
+//   console.log("no");
+// }
+
+//4. Given a number. Print "odd" if the number is odd and  "even" if it's even.
+
+let number 
+
+if (number % 2 !== 0) {
+    console.log("odd");
+}
+else if (number % 2 === 0) {
+    console.log("even");
+}
+
+
